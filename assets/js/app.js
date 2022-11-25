574a408e29b8ae0cc985042d0043f342c6b6e843
KE9SH1N <K3NSHI9@gmail.com>
2022-11-25 15:24:30 +0600

    adding mobile menu js and fixed mobile menu js design bug

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,6 @@ function hightSlide(id, hSize) {
      if (id.classList.contains("h-0")) {
           id.classList.remove("h-0", "opacity-0", "invisible");
           id.classList.add(`${hSize}`, "opacity-100", "visible");
-          console.log("cliked");
      } else {
           id.classList.add("h-0", "opacity-0", "invisible");
           id.classList.remove(`${hSize}`, "opacity-100", "visible");
@@ -15,3 +14,19 @@ $('.counter').counterUp({
      delay: 20,
      time: 2000
 });
+
+
+//DROPDOWN MOBILE MENU
+
+$(document).ready(function () {
+     $("#navToggler").each(function (_, navToggler) {
+          var target = $(navToggler).data("target");
+          $(navToggler).on("click", function () {
+               $(target).animate({
+                    height: "toggle",
+               });
+          });
+     });
+});
+
+//DROPDOWN NESTED MENU
