fd505d71d0f65a19012c3c8fd7f3c5d48d4cd0ac
ma-foyez <mafoyez.web@gmail.com>
2022-12-02 19:26:21 +0600

    add current project filtering

--- assets/js/swiperSetting.js
+++ assets/js/swiperSetting.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-     currentProjectSwiper();
+     currentProjectSwiper('all');
      homeBannerSlider();
      footerProductSlider();
      brandLogoSwiper();
@@ -31,7 +31,7 @@ function homeBannerSlider() {
 /**
  * Current Project Slider
  */
-function currentProjectSwiper() {
+function currentProjectSwiper(item) {
      var swiper = new Swiper(".current-project", {
           slidesPerView: 1,
           spaceBetween: 1,
@@ -56,6 +56,22 @@ function currentProjectSwiper() {
                prevEl: ".swiper-button-prev",
           },
      });
+
+     // Filter Slider
+     $(".current-project button")   
+     $(".current-project button").removeClass("bg-green-500");
+     $(`#${item}`).addClass("bg-green-500 border-green-500");
+     $(`#${item}`).removeClass("bg-transparent border-white/80");
+
+     if (item == "all") {
+          $("[data-filter]").removeClass("non-swiper-slide").addClass("swiper-slide").show();
+          swiper.destroy();
+     }
+     else {
+          $(".swiper-slide").not("[data-filter='" + item + "']").addClass("non-swiper-slide").removeClass("swiper-slide").hide();
+          $("[data-filter='" + item + "']").removeClass("non-swiper-slide").addClass("swiper-slide").attr("style", null).show();
+          // swiper.destroy();
+     }
 }
 
 
